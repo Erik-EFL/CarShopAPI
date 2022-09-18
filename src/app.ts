17af4d9b6952e4364126dfcf8758e374acf0615c
erik-efl <erik.lima.f@outlook.com>
2022-09-18 20:17:48 -0300

    feat: :sparkles: descomentando index e colocando rotas no app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import route from './Routes/Export.routes';
+import errorHandler from './Middleware/error.middleware';
 
 const app = express();
+app.use(express.json());
+
+app.use('/cars', route.car);
+
+app.use(errorHandler);
 
 export default app;
