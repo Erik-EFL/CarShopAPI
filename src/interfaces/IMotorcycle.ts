15b2deec2d8f77efcbe1aad72147716ad49f981d
erik-efl <erik.lima.f@outlook.com>
2022-09-19 20:30:21 -0300

    feat(src/interfaces): :sparkles: Adicionando interface motocycle

---
+++ src/interfaces/IMotorcycle.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod';
+import { zodVehicle } from './IVehicle';
+
+const zodMotorcycle = zodVehicle.extend({
+  category: z.enum(['Street', 'Custom', 'Trail']),
+  engineCapacity: z.number({
+    required_error: 'engineCapacity is required',
+    invalid_type_error: 'engineCapacity must be a number',
+  }).int().positive().lte(2500),
+});
+
+type IMotorcycle = z.infer<typeof zodMotorcycle>;
+
+export { zodMotorcycle, IMotorcycle };
